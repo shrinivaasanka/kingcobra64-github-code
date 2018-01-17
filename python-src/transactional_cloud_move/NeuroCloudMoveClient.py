0e5a5c78385630fdb458e4e5219de493c43848fb
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2018-01-17 17:37:57 +0530

    ---------------------------------------------------------------------------------------------------------------------------------------- KingCobra64 Neuro Electronic Currency transactional cloud move - Perfect Forward - 17 January 2018 ---------------------------------------------------------------------------------------------------------------------------------------- (#) Neuro Currency cloud perfect forward has been made transactional by wrapping it by Python Transaction Manager (widely used in Zope Python Application Server) (#) imports transaction python package and invokes begin() and commit() on subprocess call to neuro cloud move client and server

---
+++ python-src/transactional_cloud_move/NeuroCloudMoveClient.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------------------------------
+#NEURONRAIN ASFER - Software for Mining Large Datasets
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#--------------------------------------------------------------------------------------------------------
+#K.Srinivasan
+#NeuronRain Documentation and Licensing: http://neuronrain-documentation.readthedocs.io/en/latest/
+#Personal website(research): https://sites.google.com/site/kuja27/
+#--------------------------------------------------------------------------------------------------------
+import subprocess
+import sys
+import transaction
+
+with transaction.manager:
+	transaction.begin()
+	subprocess.call(["/home/shrinivaasanka/Krishna_iResearch_OpenSource/GitHub/asfer-github-code/cpp-src/cloud_move/asfercloudmoveclient"], shell=True)
+	transaction.commit()
