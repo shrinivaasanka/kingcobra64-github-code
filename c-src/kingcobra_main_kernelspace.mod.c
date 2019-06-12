65b8d957f565a141c7febf70d13f587be82e1a6c
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2019-06-12 16:09:45 +0530

    --------------------------------------------------------------------------------------------------------------------------------------------- Concurrent Managed Workqueue(CMWQ), VIRGO64 Queueing and KingCobra64 messaging - 12 June 2019 --------------------------------------------------------------------------------------------------------------------------------------------- 1. Existing workqueue underneath VIRGO64 queueing and requests routed by it to KingCobra64 messaging are old legacy workqueues which have been revamped to Concurrent Managed Workqueue which supports concurrent messaging and lot of other options in queue creation. 2. create_workqueue() in VIRGO64 Queueing has been changed to alloc_workqueue() of Concurrent Managed Workqueue. 3. VIRGO64 Queueing request routing to KingCobra64 messaging has been tested with CMWQ and queueing log and kingcobra64 Request-Reply Queue have been committed to respective testlogs of the drivers 4. reading from stream has been disabled in virgo_kernel_analytics.h 5. Reference - CMWQ documentation - https://www.kernel.org/doc/html/v4.11/core-api/workqueue.html 6. Byzantine Fault Tolerance in KingCobra64 persisted queue can be made available by performant CMWQ and routing to the Replicas of REQUEST_REPLY.queue by any of the practical BFT protocols available. 7. Most important application of CMWQ based VIRGO64-KingCobra64 is in the context of kernelspace hardware messaging in IoT,Drones and other analytics driven embedded systems. 8. An example usecase which is a mix of sync and async I/O in kernelspace:         (*) Analytics Variables computed by userspace machine learning are read over socket stream by kernel_analytics driver and exported kernelwide         (*) Some interested Drone driver in kernel (example PXRC) reads the analytics variables synchronously and sends reply messages asynchronously to VIRGO64 Queuing driver over kernel sockets.         (*) VIRGO Queuing routes the queued messages to KingCobra64 driver

--- c-src/kingcobra_main_kernelspace.mod.c
+++ c-src/kingcobra_main_kernelspace.mod.c
@@ -15,41 +15,6 @@ __attribute__((section(".gnu.linkonce.this_module"))) = {
 	.arch = MODULE_ARCH_INIT,
 };
 
-static const struct modversion_info ____versions[]
-__used
-__attribute__((section("__versions"))) = {
-	{ 0x939d23c1, __VMLINUX_SYMBOL_STR(module_layout) },
-	{ 0x52da9824, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
-	{ 0xd22ce214, __VMLINUX_SYMBOL_STR(kernel_sendmsg) },
-	{ 0x779a18af, __VMLINUX_SYMBOL_STR(kstrtoll) },
-	{ 0x73475f93, __VMLINUX_SYMBOL_STR(__asan_handle_no_return) },
-	{ 0xbc04f491, __VMLINUX_SYMBOL_STR(sock_release) },
-	{ 0xc33ea2fc, __VMLINUX_SYMBOL_STR(filp_close) },
-	{ 0x13f5e4f9, __VMLINUX_SYMBOL_STR(sock_create_kern) },
-	{ 0x85df9b6c, __VMLINUX_SYMBOL_STR(strsep) },
-	{ 0x91715312, __VMLINUX_SYMBOL_STR(sprintf) },
-	{ 0xc499ae1e, __VMLINUX_SYMBOL_STR(kstrdup) },
-	{ 0xc32f2bf0, __VMLINUX_SYMBOL_STR(kernel_setsockopt) },
-	{ 0x66f53979, __VMLINUX_SYMBOL_STR(kernel_connect) },
-	{ 0x57d0bb67, __VMLINUX_SYMBOL_STR(current_task) },
-	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
-	{ 0xeb2006be, __VMLINUX_SYMBOL_STR(__asan_unregister_globals) },
-	{ 0xa7eedcc4, __VMLINUX_SYMBOL_STR(call_usermodehelper) },
-	{ 0xfbd0896e, __VMLINUX_SYMBOL_STR(__asan_store8_noabort) },
-	{ 0x3122bf1a, __VMLINUX_SYMBOL_STR(init_net) },
-	{ 0x8dcfe1d8, __VMLINUX_SYMBOL_STR(__asan_register_globals) },
-	{ 0x952664c5, __VMLINUX_SYMBOL_STR(do_exit) },
-	{ 0xdb7305a1, __VMLINUX_SYMBOL_STR(__stack_chk_fail) },
-	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
-	{ 0x7ece895f, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
-	{ 0xfedfa937, __VMLINUX_SYMBOL_STR(kernel_recvmsg) },
-	{ 0x93ecf2d5, __VMLINUX_SYMBOL_STR(fd_install) },
-	{ 0xfd5d0082, __VMLINUX_SYMBOL_STR(__asan_load8_noabort) },
-	{ 0x9f1f9bbb, __VMLINUX_SYMBOL_STR(vfs_write) },
-	{ 0xe914e41e, __VMLINUX_SYMBOL_STR(strcpy) },
-	{ 0xd6227451, __VMLINUX_SYMBOL_STR(filp_open) },
-};
-
 static const char __module_depends[]
 __used
 __attribute__((section(".modinfo"))) =
